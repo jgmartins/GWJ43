d50d888017693b66a0562fa8539890e8e446c080
João Martins <anvilfolk@gmail.com>
2022-03-12 18:36:01 -0500

    Added a goat double-jump

--- Character.gd
+++ Character.gd
@@ -30,22 +30,41 @@ export (float, 0, 200) var MAX_FALL_V = 10
 export (float, 0, 500) var DF_BRAKE_AIR = 50
 export (bool) var df_wings_deployed = false
 
+# GOAT DOUBLE JUMP TIME
+export (bool) var GOAT_MODE = true
+export (int, 0, 3) var GT_NUM_JUMPS = 1
+export (int) var jumps_left = 0
+
 # Declare member variables here. Examples:
 var v : Vector2 = Vector2(0,0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	# If the snap vector hits the ground, the character will remain in contact with the ground
 	var SNAP_VECTOR := Vector2.DOWN * 100
 
 	if Input.is_action_just_pressed("ui_up"):
 		if is_on_floor():
+			# If the snap vector is nothing, it does not hit the ground
+			# and it allows the character to jump
 			SNAP_VECTOR = Vector2()
+			# Set basic jump speed
+			v.y = -JUMP
+			if GOAT_MODE:
+				jumps_left = GT_NUM_JUMPS
+			if CRICKET_MODE:
+				v.y -= CRICKET_JUMP
+		elif GOAT_MODE and jumps_left > 0: # not on floor, goat mode and jumps left!'
+			jumps_left = jumps_left - 1
 			v.y = -JUMP
 			if CRICKET_MODE:
 				v.y -= CRICKET_JUMP
-		elif DRAGONFLY_MODE:
-			df_wings_deployed = not df_wings_deployed
+			
+	
+	
+	if Input.is_action_just_pressed("ui_down") and not is_on_floor():
+		df_wings_deployed = not df_wings_deployed
 	
 	
 	if Input.is_action_pressed("ui_left"):
